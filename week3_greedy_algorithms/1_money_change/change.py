0e16030109629241b48bb6fda3ae5ca1e1aeb423
artsiom.kotau <Artsiom.Kotau@itechart-group.com>
2020-06-01 13:45:37 +0300

    Fix commit

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -2,8 +2,16 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    n = 0
+    while m > 0:
+        if m >= 10:
+            m -= 10
+        elif m >= 5:
+            m -= 5
+        else:
+            m -= 1
+        n += 1
+    return n
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- week3_greedy_algorithms/1_money_change/test_money_change.py
+++ week3_greedy_algorithms/1_money_change/test_money_change.py
@@ -1,10 +1,27 @@
 import unittest
-
+from change import get_change
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_1(self):
+        self.assertEqual(1, get_change(1))
 
+    def test_5(self):
+        self.assertEqual(1, get_change(5))
+
+    def test_10(self):
+        self.assertEqual(1, get_change(10))
+
+    def test_4(self):
+        self.assertEqual(4, get_change(4))
+
+    def test_20(self):
+        self.assertEqual(2, get_change(20))
+
+    def test_2(self):
+        self.assertEqual(2, get_change(2))
+
+    def test_28(self):
+        self.assertEqual(6, get_change(28))
 
 if __name__ == '__main__':
     unittest.main()
