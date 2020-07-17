090a808bce30492d2451d7a08319c97eccbeb262
artsiom.kotau <Artsiom.Kotau@itechart-group.com>
2020-07-17 18:27:26 +0300

    Data Structures week 3: solve hash chain

--- data_structures/week3_hash_tables/2_hash_chains/hash_chains.py
+++ data_structures/week3_hash_tables/2_hash_chains/hash_chains.py
@@ -1,4 +1,5 @@
 # python3
+from collections import deque
 
 class Query:
 
@@ -17,7 +18,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [deque() for i in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -25,33 +26,66 @@ class QueryProcessor:
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
+    def add(self, value):
+        hash_value = self._hash_func(value)
+        bucket = self.elems[hash_value]
+        contains = False
+        for item in bucket:
+            contains = item == value
+            if contains:
+                break
+
+        if not contains:
+            bucket.appendleft(value)
+
+    def remove(self, value):
+        hash_value = self._hash_func(value)
+        bucket = self.elems[hash_value]
+        try:
+            bucket.remove(value)
+        except ValueError:
+            pass
+
+    def find(self, value):
+        hash_value = self._hash_func(value)
+        bucket = self.elems[hash_value]
+        result = False
+        for item in bucket:
+            result = item == value
+            if result:
+                break
+        return result
+
+    def check(self, i):
+        values = self.elems[i]
+        if values is None:
+            values = deque()
+        return values
+
+
     def write_search_result(self, was_found):
         print('yes' if was_found else 'no')
 
     def write_chain(self, chain):
-        print(' '.join(chain))
+        array = []
+        if chain:
+            for item in chain:
+                array.append(item)
+        print(' '.join(array))
 
     def read_query(self):
         return Query(input().split())
 
     def process_query(self, query):
         if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(self.check(query.ind))
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                self.write_search_result(self.find(query.s))
             elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+                self.add(query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                self.remove(query.s)
 
     def process_queries(self):
         n = int(input())
